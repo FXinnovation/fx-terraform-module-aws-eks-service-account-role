bac385dff37b563f54a34677bc5d94cea95e5080
Kim Samba <kim.samba@fxinnovation.com>
2022-06-01 15:32:41 -0400

    fix: use correct output

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@
 
 output "kubernetes_annotation" {
   description = "Kubernetes annoations to add on the service account for it to be able to assume the role."
-  value       = { "eks.amazonaws.com/role-arn" = element(concat(aws_iam_role.this.*.arn, list("")), 0) }
+  value       = { "eks.amazonaws.com/role-arn" = element(concat(aws_iam_role.this.*.arn, [""]), 0) }
 }
 
 
@@ -13,17 +13,17 @@ output "kubernetes_annotation" {
 #####
 
 output "iam_role_name" {
-  value = element(concat(aws_iam_role.this.*.name, list("")), 0)
+  value = element(concat(aws_iam_role.this.*.name, [""]), 0)
 }
 
 output "iam_role_arn" {
-  value = element(concat(aws_iam_role.this.*.arn, list("")), 0)
+  value = element(concat(aws_iam_role.this.*.arn, [""]), 0)
 }
 
 output "iam_role_id" {
-  value = element(concat(aws_iam_role.this.*.id, list("")), 0)
+  value = element(concat(aws_iam_role.this.*.id, [""]), 0)
 }
 
 output "iam_role_unique_id" {
-  value = element(concat(aws_iam_role.this.*.unique_id, list("")), 0)
+  value = element(concat(aws_iam_role.this.*.unique_id, [""]), 0)
 }
